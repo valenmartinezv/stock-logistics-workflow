56946978175daf0a3e9b290a06b501dfe9792f09
Valentina <123267856+valenmartinezv@users.noreply.github.com>
2024-02-23 14:27:40 -0300

    Delete stock_picking_batch_extended_account_sale_type directory

--- stock_picking_batch_extended_account_sale_type/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import sale_order_type
-from . import stock_batch_picking
--- stock_picking_batch_extended_account_sale_type/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright 2022 Sergio Teruel - Tecnativa <sergio.teruel@tecnativa.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import test_stock_picking_batch_extended_account_sale_type
